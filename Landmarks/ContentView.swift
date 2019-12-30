540e375311092a31df7fb04daab4231c7850dd99
sxc <qiuyequ@icloud.com>
2019-12-30 14:38:52 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Xiaochun Shen on 2019/12/30.
+//  Copyright © 2019 SXC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
